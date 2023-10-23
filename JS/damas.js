0135941d64f2a812a3033e0d7a7d78ba46b35137
aldara <aldarafernandezcarro@gmail.com>
2023-10-23 12:23:02 +0200

    Se añade la función para seleccionar las celdas y se plantean otras funciones por desarrollar

--- JS/damas.js
+++ JS/damas.js
@@ -1,4 +1,5 @@
 const imagenes = document.getElementsByTagName("img")
+const boton = document.getElementById("nuevaPartida")
 
 let turno = document.getElementById("turnoJugador")
 
@@ -22,18 +23,22 @@ let tablero = [
     [VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO]
 ] 
 
+
+
+
+// Actualiza el tablero a la posición inicial de las fichas
 let fichasIniciales = () => {
     for (const imagen of imagenes) {
         if(imagen.src == "/img/negra.png"){
             const [ ,fila, columna] = imagen.id.split("_")
-            tablero[fila, columna] = NEGRAS
+            tablero[fila][columna] = NEGRAS
         }else if(imagen.src == "/img/blanca.png"){
             const [ ,fila, columna] = imagen.id.split("_")
-            tablero[fila, columna] = BLANCAS
+            tablero[fila][columna] = BLANCAS
         }
     }
 }
-
+// Cambia el turno actual
 let cambiarTurno = () => {
     if(turnoJugador == JUGADOR1){
         turnoJugador = JUGADOR2
@@ -43,3 +48,34 @@ let cambiarTurno = () => {
         turno.innerText = "Jugador 2 / Negras"
     }
 }
+let moverFicha = () => {
+
+}
+let seleccionarFicha = event => {
+    const [, fila, columna] = event.target.id.split("_")
+    console.log(event.target.id.split("_"))
+    console.log(tablero[fila][columna])
+    if (tablero[fila][columna] === NEGRAS || tablero[fila][columna] === BLANCAS){
+        console.log(event.target.parentElement)
+        event.target.parentElement.style.border = "red 3px solid"
+    }
+}
+let nuevoJuego = () => {
+    fichasIniciales()
+}
+
+
+
+
+
+
+
+
+fichasIniciales()
+for (const imagen of imagenes) {
+    if(imagen.src != "/img/vacio.png"){
+        imagen.onclick = seleccionarFicha
+    }
+    
+}
+boton.onclick = nuevoJuego
