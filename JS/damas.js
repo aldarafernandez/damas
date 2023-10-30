a2470f307d28b7bd14a2ad269f969595bf90d87b
aldara <aldarafernandezcarro@gmail.com>
2023-10-30 10:58:45 +0100

    Se añade a la funcion seleccionar() que compruebe si la ficha escogida se corresponde con la del turno. Se añade el array del tablero para que actualice las posiciones de las fichas

--- JS/damas.js
+++ JS/damas.js
@@ -15,17 +15,38 @@ let turno = document.getElementById("turnoJugador")
 let idFichaSeleccionada = ""
 let idDestino = ""
 
+// Posiciones iniciales de las fichas
+let tableroinicial = [
+    [VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2],
+    [JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO],
+    [VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2],
+    [VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO],
+    [VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO],
+    [JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO],
+    [VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1],
+    [JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO]
+] 
+// Tablero con las posiciones iniciales de la fichas que cambia a medida que se mueven
+let tablero = [
+    [VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2],
+    [JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO],
+    [VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2],
+    [VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO],
+    [VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO, VACIO],
+    [JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO],
+    [VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1],
+    [JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO]
+] 
 
 
-
-
-// Actualiza el tablero a la posición inicial de las fichas
-let fichasIniciales = () => {
+// Actualiza el tablero a la posición actual de las fichas
+let fichasTablero = () => {
     for (const imagen of imagenes) {
-        if(imagen.src == "/img/negra.png"){
+        let fichaImagen = imagen.getAttribute("src")
+        if(fichaImagen === "/img/negra.png"){
             const [ ,fila, columna] = imagen.id.split("_")
             tablero[fila][columna] = NEGRAS
-        }else if(imagen.src == "/img/blanca.png"){
+        }else if(fichaImagen === "/img/blanca.png"){
             const [ ,fila, columna] = imagen.id.split("_")
             tablero[fila][columna] = BLANCAS
         }
@@ -33,14 +54,14 @@ let fichasIniciales = () => {
 }
 // Cambia el turno actual
 let cambiarTurno = () => {
-    if(turnoJugador == JUGADOR1){
+    if(turnoJugador === JUGADOR1){
         turnoJugador = JUGADOR2
-        turno.innerText = "Jugador 1 / Blancas"
+        turno.innerText = "Jugador 2 / Negras"
         idDestino = ""
         idFichaSeleccionada = ""
     }else{
         turnoJugador = JUGADOR1
-        turno.innerText = "Jugador 2 / Negras"
+        turno.innerText = "Jugador 1 / Blancas"
         idDestino = ""
         idFichaSeleccionada = ""
     }
@@ -48,41 +69,74 @@ let cambiarTurno = () => {
 // mueve la ficha a la posición seleccionada
 let moverFicha = (posicionOriginal, nuevaPosicion) => {
     let celdaInicial = document.getElementById(posicionOriginal)
-    console.log(celdaInicial)
     let celdaVacia = document.getElementById(nuevaPosicion)
-    console.log(celdaVacia)
     celdaVacia.removeAttribute("src")
     celdaVacia.setAttribute("src", celdaInicial.src)
     celdaVacia.src = celdaInicial.src
     celdaInicial.src = "/img/vacio.png"
     celdaInicial.parentElement.style.removeProperty("border")
-    celdaVacia.parentElement.style.removeProperty("border")
+    fichasTablero()
     cambiarTurno()
 }
 // selecciona la ficha a mover y la posición a la que se mueve
 let seleccionar = event => {
     if (idFichaSeleccionada === ""){
-        idFichaSeleccionada = event.target.id
-        event.target.parentElement.style.border = "3px solid red"
+        let seleccionada = document.getElementById(event.target.id)
+        let ficha = seleccionada.getAttribute("src")
+        if (turnoJugador === JUGADOR1){
+            if (ficha === "/img/blanca.png"){
+                idFichaSeleccionada = event.target.id
+                event.target.parentElement.style.border = "3px solid red"
+            }
+        }else {
+            if (ficha === "/img/negra.png"){
+                idFichaSeleccionada = event.target.id
+                event.target.parentElement.style.border = "3px solid red"
+            }
+        }
+        
     }else if(idDestino === ""){
-        idDestino = event.target.id
-        event.target.parentElement.style.border = "3px solid red"
+        let destino = document.getElementById(event.target.id)
+        let ficha = destino.getAttribute("src")
+        if (ficha !== "/img/blanca.png" && ficha !== "/img/negra.png" ){
+            idDestino = event.target.id
+        }
     }
-
     if (idFichaSeleccionada !== "" && idDestino !== ""){
         moverFicha(idFichaSeleccionada, idDestino)
     }
 }
+// Comprueba el ganador mirando si no queda ninguna ficha de su color
+let comprobarGanador = () => {
+    let fichasBlancasIniciales = 12
+    let fichasNegrasIniciales = 12
+    let fichasBlancas = 0
+    let fichasNegras = 0
+    for (const imagen of imagenes) {
+        let fichaimagen = imagen.getAttribute("src")
+        if(fichaimagen == "/img/blanca.png"){
+            fichasBlancas++
+        }else if(fichaimagen == "/img/negra.png"){
+            fichasNegras++
+        }
+    }
+
+    if (fichasBlancasIniciales - fichasBlancas === 0){
+        alert("Gana el Jugador 2")
+        nuevoJuego()
+    }else if (fichasNegrasIniciales - fichasNegras === 0){
+        alert("Gana el Jugador 1")
+        nuevoJuego()
+    }
+}
 // reinicia el juego  
 let nuevoJuego = () => {
-    fichasIniciales()
+    tablero = tableroinicial
     turno.innerText = "Jugador 1 / Blancas"
     idDestino = ""
     idFichaSeleccionada = ""
     for (const imagen of imagenes) {
-        if(imagen.src !== "/img/vacio.png"){
-            imagen.onclick = seleccionar
-        }    
+        imagen.onclick = seleccionar   
     }
     boton.onclick = nuevoJuego
 }
@@ -91,14 +145,8 @@ let nuevoJuego = () => {
 
 
 
-
-
-
-fichasIniciales()
 turno.innerText = "Jugador 1 / Blancas"
 for (const imagen of imagenes) {
-    if(imagen.src !== "/img/vacio.png"){
-        imagen.onclick = seleccionar
-    }    
+    imagen.onclick = seleccionar  
 }
 boton.onclick = nuevoJuego
