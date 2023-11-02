636718135c678156031d71a00a4163bad7cda9ca
aldara <aldarafernandezcarro@gmail.com>
2023-11-02 09:52:26 +0100

    Se soluciona que las fichas se muevan correctamente en cada turno

--- JS/damas.js
+++ JS/damas.js
@@ -1,22 +1,22 @@
-const imagenes = document.getElementsByTagName("img")
-const boton = document.getElementById("nuevaPartida")
+const imagenes = document.getElementsByTagName("img");
+const boton = document.getElementById("nuevaPartida");
 
 
-const BLANCAS = 1
-const NEGRAS = 2
-const VACIO = 0
+const BLANCAS = 1;
+const NEGRAS = 2;
+const VACIO = 0;
 
-const JUGADOR1 = 1
-const JUGADOR2 = 2
+const JUGADOR1 = 1;
+const JUGADOR2 = 2;
 
-let turnoJugador = JUGADOR1
-let turno = document.getElementById("turnoJugador")
+let turnoJugador = JUGADOR1;
+let turno = document.getElementById("turnoJugador");
 
-let idFichaSeleccionada = ""
-let idDestino = ""
+let idFichaSeleccionada = "";
+let idDestino = "";
 
 // Posiciones iniciales de las fichas
-let tableroinicial = [
+let tableroInicial = [
     [VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2],
     [JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO],
     [VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2],
@@ -25,7 +25,7 @@ let tableroinicial = [
     [JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO],
     [VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1],
     [JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO]
-] 
+]; 
 // Tablero con las posiciones iniciales de la fichas que cambia a medida que se mueven
 let tablero = [
     [VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2, VACIO, JUGADOR2],
@@ -36,117 +36,158 @@ let tablero = [
     [JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO],
     [VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1],
     [JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO, JUGADOR1, VACIO]
-] 
+]; 
 
 
 // Actualiza el tablero a la posición actual de las fichas
 let fichasTablero = () => {
     for (const imagen of imagenes) {
-        let fichaImagen = imagen.getAttribute("src")
+        let fichaImagen = imagen.getAttribute("src");
         if(fichaImagen === "/img/negra.png"){
-            const [ ,fila, columna] = imagen.id.split("_")
-            tablero[fila][columna] = NEGRAS
+            const [ ,fil, col] = imagen.id.split("_");
+            let fila = +fil;
+            let columna = +col;
+            tablero[fila][columna] = NEGRAS;
         }else if(fichaImagen === "/img/blanca.png"){
-            const [ ,fila, columna] = imagen.id.split("_")
-            tablero[fila][columna] = BLANCAS
+            const [ ,fil, col] = imagen.id.split("_");
+            let fila = +fil;
+            let columna = +col;
+            tablero[fila][columna] = BLANCAS;
         }
     }
 }
 // Cambia el turno actual
 let cambiarTurno = () => {
     if(turnoJugador === JUGADOR1){
-        turnoJugador = JUGADOR2
-        turno.innerText = "Jugador 2 / Negras"
-        idDestino = ""
-        idFichaSeleccionada = ""
+        turnoJugador = JUGADOR2;
+        turno.innerText = "Jugador 2 / Negras";
+        idDestino = "";
+        idFichaSeleccionada = "";
     }else{
-        turnoJugador = JUGADOR1
-        turno.innerText = "Jugador 1 / Blancas"
-        idDestino = ""
-        idFichaSeleccionada = ""
+        turnoJugador = JUGADOR1;
+        turno.innerText = "Jugador 1 / Blancas";
+        idDestino = "";
+        idFichaSeleccionada = "";
     }
 }
-// mueve la ficha a la posición seleccionada
+// mueve la ficha a la posición seleccionada comprobando que sea un movimiento válido
 let moverFicha = (posicionOriginal, nuevaPosicion) => {
-    let celdaInicial = document.getElementById(posicionOriginal)
-    let celdaVacia = document.getElementById(nuevaPosicion)
-    celdaVacia.removeAttribute("src")
-    celdaVacia.setAttribute("src", celdaInicial.src)
-    celdaVacia.src = celdaInicial.src
-    celdaInicial.src = "/img/vacio.png"
-    celdaInicial.parentElement.style.removeProperty("border")
-    fichasTablero()
-    cambiarTurno()
+    let celdaInicial = document.getElementById(posicionOriginal);
+    let celdaVacia = document.getElementById(nuevaPosicion);
+    const[ , fil, col] = celdaInicial.id.split("_");
+    const fila = +fil;
+    const columna = +col;
+
+    if (celdaInicial.getAttribute("src") === "/img/blanca.png"){
+        if((nuevaPosicion === "img_"+(fila-1)+"_"+columna) || (celdaVacia.id === "img_"+(fila-1)+"_"+(columna+1)) || (celdaVacia.id === "img_"+(fila-1)+"_"+(columna-1))){
+            celdaVacia.removeAttribute("src");
+            celdaVacia.setAttribute("src", celdaInicial.src);
+            celdaInicial.src = "/img/vacio.png";
+            celdaInicial.parentElement.style.removeProperty("border");
+            fichasTablero();
+            cambiarTurno();
+            console.log(tablero)
+        }
+    }else if (celdaInicial.getAttribute("src") === "/img/negra.png"){
+        console.log(nuevaPosicion, celdaVacia.id)
+        if((nuevaPosicion === "img_"+(fila+1)+"_"+columna) || (celdaVacia.id === "img_"+(fila+1)+"_"+(columna+1)) || (celdaVacia.id === "img_"+(fila+1)+"_"+(columna-1))){
+            celdaVacia.removeAttribute("src");
+            celdaVacia.setAttribute("src", celdaInicial.src);
+            celdaInicial.src = "/img/vacio.png";
+            celdaInicial.parentElement.style.removeProperty("border");
+            fichasTablero();
+            cambiarTurno();
+            console.log(tablero)
+        }
+    }
+    
 }
 // selecciona la ficha a mover y la posición a la que se mueve
 let seleccionar = event => {
     if (idFichaSeleccionada === ""){
-        let seleccionada = document.getElementById(event.target.id)
-        let ficha = seleccionada.getAttribute("src")
+        let seleccionada = document.getElementById(event.target.id);
+        let ficha = seleccionada.getAttribute("src");
         if (turnoJugador === JUGADOR1){
             if (ficha === "/img/blanca.png"){
-                idFichaSeleccionada = event.target.id
-                event.target.parentElement.style.border = "3px solid red"
+                idFichaSeleccionada = event.target.id;
+                event.target.parentElement.style.border = "3px solid red";
             }
         }else {
             if (ficha === "/img/negra.png"){
-                idFichaSeleccionada = event.target.id
-                event.target.parentElement.style.border = "3px solid red"
+                idFichaSeleccionada = event.target.id;
+                event.target.parentElement.style.border = "3px solid red";
             }
         }
         
     }else if(idDestino === ""){
-        let destino = document.getElementById(event.target.id)
-        let ficha = destino.getAttribute("src")
+        let destino = document.getElementById(event.target.id);
+        let ficha = destino.getAttribute("src");
         if (ficha !== "/img/blanca.png" && ficha !== "/img/negra.png" ){
-            idDestino = event.target.id
+            idDestino = event.target.id;
         }
     }
     if (idFichaSeleccionada !== "" && idDestino !== ""){
-        moverFicha(idFichaSeleccionada, idDestino)
+        moverFicha(idFichaSeleccionada, idDestino);
     }
 }
 // Comprueba el ganador mirando si no queda ninguna ficha de su color
 let comprobarGanador = () => {
-    let fichasBlancasIniciales = 12
-    let fichasNegrasIniciales = 12
-    let fichasBlancas = 0
-    let fichasNegras = 0
+    let fichasBlancasIniciales = 12;
+    let fichasNegrasIniciales = 12;
+    let fichasBlancas = 0;
+    let fichasNegras = 0;
     for (const imagen of imagenes) {
-        let fichaimagen = imagen.getAttribute("src")
+        let fichaimagen = imagen.getAttribute("src");
         if(fichaimagen == "/img/blanca.png"){
-            fichasBlancas++
+            fichasBlancas++;
         }else if(fichaimagen == "/img/negra.png"){
-            fichasNegras++
+            fichasNegras++;
         }
     }
 
     if (fichasBlancasIniciales - fichasBlancas === 0){
-        alert("Gana el Jugador 2")
-        nuevoJuego()
+        alert("Gana el Jugador 2");
+        nuevoJuego();
     }else if (fichasNegrasIniciales - fichasNegras === 0){
-        alert("Gana el Jugador 1")
-        nuevoJuego()
+        alert("Gana el Jugador 1");
+        nuevoJuego();
     }
 }
 // reinicia el juego  
 let nuevoJuego = () => {
-    tablero = tableroinicial
-    turno.innerText = "Jugador 1 / Blancas"
-    idDestino = ""
-    idFichaSeleccionada = ""
+    tablero = tableroInicial
     for (const imagen of imagenes) {
-        imagen.onclick = seleccionar   
+        const [ ,fil, col] = imagen.id.split("_");
+        let fila = +fil;
+            let columna = +col;
+        if (tablero[fila, columna] === 1){
+            imagen.removeAttribute("src");
+            imagen.setAttribute("src", "/img/blanca.png");
+        }else if(tablero[fila, columna] === 2){
+            imagen.removeAttribute("src");
+            imagen.setAttribute("src", "/img/negra.png");
+        }else if(tablero[fila, columna] === 0){
+            imagen.removeAttribute("src");
+            imagen.setAttribute("src", "/img/vacio.png");
+        }
     }
-    boton.onclick = nuevoJuego
+    console.log(tablero)
+    fichasTablero();
+    turno.innerText = "Jugador 1 / Blancas";
+    idDestino = "";
+    idFichaSeleccionada = "";
+    for (const imagen of imagenes) {
+        imagen.onclick = seleccionar; 
+    }
+    boton.onclick = nuevoJuego;
 }
 
 
 
 
 
-turno.innerText = "Jugador 1 / Blancas"
+turno.innerText = "Jugador 1 / Blancas";
 for (const imagen of imagenes) {
-    imagen.onclick = seleccionar  
+    imagen.onclick = seleccionar;  
 }
-boton.onclick = nuevoJuego
+boton.onclick = nuevoJuego;
